4c1373d626644dc4896ae83760c4f40af637c4ad
Joe Krause <jkrause@e1z1r6p15.42.us.org>
2017-10-02 10:24:11 -0700

    Add tests

---
+++ tests/correction_p1.sh
@@ -0,0 +1,1 @@
+rm -f wins; for i in $(seq 5); do rm -f debug_1; ./filler_vm -p1 ../filler -p2 $1 -f $2; cat filler.trace | grep "won" >> wins; done; cat wins
---
+++ tests/correction_p2.sh
@@ -0,0 +1,1 @@
+rm -f wins; for i in $(seq 5); do rm -f debug_2; ./filler_vm -p2 ../filler -p1 $1 -f $2; cat filler.trace | grep "won" >> wins; done; cat wins
